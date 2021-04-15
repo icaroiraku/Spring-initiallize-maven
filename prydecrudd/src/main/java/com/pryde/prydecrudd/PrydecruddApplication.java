8160625eaa8452ecab7eb41c3dba5e02894d8b8a
IcaroIraku <icaroiraku@gmail.com>
2021-04-15 19:23:55 -0300

    commit

---
+++ prydecrudd/src/main/java/com/pryde/prydecrudd/PrydecruddApplication.java
@@ -0,0 +1,13 @@
+package com.pryde.prydecrudd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrydecruddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrydecruddApplication.class, args);
+	}
+
+}
