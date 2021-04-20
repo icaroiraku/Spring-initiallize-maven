df03d021140135d31033fc50158a5fef869a3868
Gabriel Vogado <43838749+GabrielVogado@users.noreply.github.com>
2021-04-19 22:24:51 -0300

    Update ZurichRepository.java
    Adição de steriotype Repository

--- prydecrudd/prydecrudd/src/main/java/com/pryde/prydecrudd/repository/ZurichRepository.java
+++ prydecrudd/prydecrudd/src/main/java/com/pryde/prydecrudd/repository/ZurichRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pryde.prydecrudd.entity.Zurich;
 
+@Repository
 public interface ZurichRepository extends JpaRepository<Zurich,Long> {
 
 }
