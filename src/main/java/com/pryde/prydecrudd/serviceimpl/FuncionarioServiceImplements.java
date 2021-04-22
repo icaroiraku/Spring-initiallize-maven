1daa2e3f9467f2590315de116921b6852d80e2ed
icaroiraku <icaroiraku@gmail.com>
2021-04-22 10:48:49 -0300

    Delete FuncionarioServiceImplements.java

--- src/main/java/com/pryde/prydecrudd/serviceimpl/FuncionarioServiceImplements.java
+++
@@ -1,9 +0,0 @@
-package com.pryde.prydecrudd.serviceimpl;
-
-import com.pryde.prydecrudd.entity.Funcionario;
-
-public interface FuncionarioServiceImplements {
-	
-	Funcionario  salvarFuncionario(Funcionario funcionario);
-
-}
